e54207a1757548464c3e4fd418778130792a164d
Gonzalo Romero <programaciongonzalitox@gmail.com>
2025-08-23 12:25:58 -0300

    feat: add error boundary

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+// src/components/ErrorBoundary.jsx
+
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error('ErrorBoundary caught an error:', error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Ocurrió un error, intenta recargar la página.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
